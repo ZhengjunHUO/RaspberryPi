87e4556817cd410a295fe7e1ed6a8bd308f5ff66
ZhengjunHUO <firelouiszj@hotmail.com>
2025-08-10 10:13:34 +0200

    shock switch

---
+++ 07_shock/shock.c
@@ -0,0 +1,39 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
+
+# define PIN_SHOCK 0   // [S] connected to GPIO17
+# define PIN_LASER 1  // [S] connected to GPIO18
+
+static volatile bool running = true;
+
+static void intr_handler(int signal) {
+	printf("Ctrl-c captured, quit ...\n");
+	running = false;
+}
+
+int main() {
+	if(wiringPiSetup() == -1) {
+		printf("Error occurred setting up wiringPi, quit...\n");
+		return -1;
+	}
+
+	pinMode(PIN_SHOCK, INPUT);
+	pinMode(PIN_LASER, OUTPUT);
+	signal(SIGINT, intr_handler);
+
+	while(running) {
+		if(digitalRead(PIN_SHOCK) == 0) {
+			printf("Shock detected.\n");
+			digitalWrite(PIN_LASER, HIGH);
+		} else {
+			printf("Idle\n");
+			digitalWrite(PIN_LASER, LOW);
+		}
+		delay(100);
+	}
+
+	printf("Bye\n");
+	return 0;
+}
