aa7da104d25711357de67237cfe41edfba300ab0
ZhengjunHUO <firelouiszj@hotmail.com>
2025-08-13 06:58:43 +0200

    test sound sensor

---
+++ 12_sound/sound.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <adc0832.h>
+
+#define PIN_DATA_IN 0	// DO(6) -> CPIO17
+#define PIN_DATA_OUT 1	// DI(5) -> CPIO18
+#define PIN_CLK 14		// CLK(7) -> SCLK
+#define PIN_CS 15		// CS(1)-> TXD
+						// CH0(2) -> [S]
+				
+int main() {
+	uchar val;
+	
+	if (wiringPiSetup() == -1) {
+		printf("wiringPi setup failed!\n");
+		exit(1);
+	}
+
+	adc0832_setup(
+		PIN_DATA_IN,
+		PIN_DATA_OUT,
+		PIN_CS,
+		PIN_CLK
+	);
+	
+	while (1) {
+		val = adc0832_read(
+			PIN_DATA_IN,
+			PIN_DATA_OUT,
+			PIN_CS,
+			PIN_CLK,
+			adc0832_mode_single_ended,
+			adc0832_channel0
+		);
+		printf("soud value: %d\n", val);
+
+		delay(100);
+	}
+	return 0;
+}
